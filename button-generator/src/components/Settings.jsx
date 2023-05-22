eb6b6d4351f3f0a926a7c9a1b12f4e016099e977
Eliswer <106431426+Eliswer@users.noreply.github.com>
2023-05-23 01:05:24 +0200

    ++

--- button-generator/src/components/Settings.jsx
+++ button-generator/src/components/Settings.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Header from "./Header";
 import classes from "../styles/settings.module.css";
 import drawing from "../icons/Drawing.svg";
@@ -6,12 +7,42 @@ import Title from "./Title";
 import TextInside from "./settings/TextInside";
 
 function Settings() {
+  const [isChecked, setIsChecked] = useState(false);
+
+  const checkHandler = () => {
+    setIsChecked(!isChecked);
+  };
+
   return (
     <div>
       <Header icon={drawing} title={"Style your button"} />
       <div className={classes["settings-wrapper"]}>
         <General />
         <TextInside />
+        <div className={classes.s3}>
+          <Title title={"Border"} />
+          <div className={classes.line}>
+            <p>Enable:</p>
+            <input
+              type="checkbox"
+              checked={isChecked}
+              onChange={checkHandler}
+            />
+          </div>
+          {isChecked ? (
+            <div className={classes.enabled}>
+              <div className={classes.line}>
+                <p>Font:</p>
+                <select name="cars" id="cars">
+                  <option value="volvo">Solid</option>
+                  <option value="saab">**</option>
+                </select>
+              </div>
+            </div>
+          ) : (
+            ""
+          )}
+        </div>
       </div>
     </div>
   );
